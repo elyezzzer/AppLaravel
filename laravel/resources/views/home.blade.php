aa446d1e48609417b1223b019de71a5ac977ff55
elyezzzer <elyezzer1247@gmail.com>
2025-10-04 16:07:28 -0300

    Setup inicial: configuração do laravel, blade e rotas /home

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="py-4 flex justify-center items-center">
+        <div class="py-6 px-6">
+            <div class="mx-auto sm:px-6 lg:px-8 text-center">
+                <h1 class="text-2xl font-sans mb-2">Bem vindo ao meu App</h1> 
+        
+            </div>
+        </div>
+    </div> 
+</x-app-layout>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        // se o usuario esta logado vai para /home
+        return redirect()->route('home');
+    }
+    // se nao esta logado mostra a pagina de login
+    return redirect()->route('login');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+require __DIR__.'/auth.php';
